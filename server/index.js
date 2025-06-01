7542c188025cbf242ae10ff0a4c9a214e71d1169
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2025-06-01 17:02:25 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -9,11 +9,14 @@ const PORT = process.env.PORT || 5000;
 
 
 // Enable CORS
-app.use(cors({
-  origin: 'https://ai-girlfriend-eta.vercel.app/', // Replace with your frontend origin
-   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'https://ai-girlfriend-eta.vercel.app/', // Replace with your frontend origin
+//    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+// }));
+
+app.options('*', cors());
+
 
 app.use(express.json())
 app.use('/api', chatRoute);
